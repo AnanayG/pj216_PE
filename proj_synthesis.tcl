e6be5a8a61b57b953116a0cddaac985e94066b1f
Ananay <ananay98@gmail.com>
2022-11-17 18:50:52 -0800

    minor changes

--- proj_synthesis.tcl
+++ proj_synthesis.tcl
@@ -26,7 +26,7 @@ set_operating_conditions -min ff1p16vn40c -max ss0p95v125c
 
 
 # Describe the clock waveform & setup operating conditions
-set Tclk 8
+set Tclk 1.51
 set TCU  0.1
 set IN_DEL 0.6
 set IN_DEL_MIN 0.3
